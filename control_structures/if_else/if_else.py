6eb85a32a02245c62afebf8e2d5d88d7ec8ee757
Raman Aggarwal <113297581+ramanagg@users.noreply.github.com>
2023-05-07 07:08:39 +0000

    if_else basics

---
+++ control_structures/if_else/if_else.py
@@ -0,0 +1,6 @@
+# program to check numer is even or odd
+x=124569878
+if((x%2) == 0):
+    print('even')
+else:
+    print('odd')
---
+++ control_structures/if_else/postive_negative.py
@@ -0,0 +1,7 @@
+x=0
+if(x>0):
+    print('Postive')
+elif(x==0):
+    print('Equals to Zero')    
+else:
+    print('Negative')
